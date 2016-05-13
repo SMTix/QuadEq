d13e3f02b12917300a0d70d7fa22a594c3bdf777
SMTix <SMTix@yandex.ru>
2016-05-13 10:31:31 +0300

    Create a main program

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <math.h>
 
+
+int main () {
+    float a, b, c;
+    printf( "Input coefficient's of equation (a, b, c)\n" );
+    scanf( "%f %f %f", &a, &b, &c );
+    process( a, b, c );
+    return 0;
+}
